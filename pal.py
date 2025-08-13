0329640453bf6aa9a8af3287c568773997fc1b70
Parvar99 <89199902+Parvar99@users.noreply.github.com>
2025-08-13 16:25:37 +0530

    Update pal.py
    changes made

--- pal.py
+++ pal.py
@@ -2,7 +2,7 @@ def is_palindrome(s):
     cleaned_str = ''.join(s.split()).lower()
     return cleaned_str == cleaned_str[::-1]
 
-test_strings = ["racecar", "hello", "A man a plan a canal Panama", "madam", "12321"]
+test_strings = ["racecar", "hello", "A man a plan a canal Panama", "madam", "12121"]
 
 for test_string in test_strings:
     if is_palindrome(test_string):
